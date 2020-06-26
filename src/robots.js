1a3d39df031a357f3d6355cf17ba26e9f50414c3
krishna25622 <64345473+krishna25622@users.noreply.github.com>
2020-06-26 19:27:36 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,29 @@
+export const robots =[
+{
+	id:1,
+	name:'krishna',
+	username:'K',
+	email:'kkp.krishu@gmail.com'
+},
+{
+	id:2,
+	name:'Kris',
+	username:'K',
+	email:'kkp.krishu@yahooo.com'
+
+},
+{
+	id: 3,
+	name:'Krish',
+	username:'k',
+	email:'kkp.krishu@outlook.com'
+
+},
+{
+	id: 4,
+	name:'Krishan',
+	username:'k',
+	email:'kkp.krish@outlook.com'
+
+}
+]
